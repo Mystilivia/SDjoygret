22b67b1ddeb65d23ae47180ef6d066a2d3a969d4
Sylvain Dechaumet <mystilivia@gmail.com>
2016-03-08 21:02:44 +0100

    Testing

---
+++ R/Package.1_functions.R
@@ -0,0 +1,15 @@
+#' First function
+#'
+#' This function is just a test for R package creation.
+#' @param x Argument for [...]
+#' @keywords Test
+#' @export
+#' @examples
+#' testing_function_01(1)
+testing_function_01 <- function(x){
+  if(x==1){
+    print("Yeah ! you typed 1")
+  } else {
+    print("I don't get it...")
+  }
+}
