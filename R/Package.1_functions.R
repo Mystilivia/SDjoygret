7d1315c07f28f40bf3db67d05c95bd75db4f0b30
Mystilivia <mystilivia@gmail.com>
2016-03-09 17:19:25 +0100

    First full function

--- R/Package.1_functions.R
+++ R/Package.1_functions.R
@@ -1,16 +1,138 @@
 #' First function
 #'
 #' This function is just a test for R package creation.
-#' @param x Argument for [...]
+#' @param x Must be numeric
 #' @keywords Test
 #' @export
 #' @examples
 #' testing_function_01(1)
 
 testing_function_01 <- function(x){
+  if (is.numeric(x)==FALSE){
+    stop("Please give me numeric to eat !")
+  }
   if(x==1){
     print("Yeah ! you typed 1")
+    result.step1 <- x*5
+    result.step2 <- x/2
+    list.result <- list(result.step1, result.step2)
+    return(list.result)
   } else {
     print("I don't get it...")
   }
 }
+
+#' xcms_orbi_GRT
+#'
+#' This function simplify workflow from peak grouping and retention time correction
+#' to PCA analysis. Function will perform group -> retcor -> group -> retcor -> group
+#' and return EIC for standards across all samples and extract standards from peaks table,
+#' giving also ppm deviation for each std for diagnostic.
+#' @param xcms_set_obj xcmsset object (result of peak picking)
+#' @param Results.dir.name Name of the subfolder to store results
+#' @param bw_param Vector for bw settings to use in 1, 2 and 3 iteration : c(1, 2, 3)
+#' @param mzwid_param mzwid parameter to use
+#' @param minfrac_param minfrac parameter to use
+#' @param profStep_param profStep parameter to use
+#' @param Sample.Metadata Dataframe with samples names and metadata
+#' @param Grouping.factor Col.number of Sample.Metadata to use for ACP colors (can be a vector)
+#' @keywords xcms, orbitrap
+#' @usage xcms_orbi_GRT(xcms_set_obj, Results.dir.name="Default", bw_param=c(25, 10, 0.7), mzwid_param=0.005, minfrac_param=0.25, profStep_param=0.8)
+#' xcms_orbi_GRT()
+#' @export
+
+xcms_orbi_GRT <- function(xcms_set_obj, Results.dir.name="Default", bw_param=c(15, 8, 0.8), mzwid_param=0.005, minfrac_param=0.7, profStep_param=0.5, Sample.Metadata, Grouping.factor=1){
+  ## Package requirement
+  require("xcms")
+  require("ropls")
+
+  ## Create directory and path
+  Results.path.root <- paste0("./",Results.dir.name,"/")
+  Results.path.rtgraph <- paste0(Results.path.root, "/RetCor_Dev/")
+  Results.path.pca <- paste0(Results.path.root, "/PCA/")
+  dir.create(Results.path.root, showWarnings = F)
+  dir.create(Results.path.rtgraph, showWarnings = F)
+  dir.create(Results.path.pca, showWarnings = F)
+
+  xset.group <- xcms::group(xset.default, method="density", bw=bw_param[1], mzwid=mzwid_param, minfrac=mzwid_param)
+  xset.2 <- xcms::retcor(xset.group, method="obiwarp", profStep=profStep_param, plottype="deviation")
+  dev.copy(png, paste0(Results.path.rtgraph, "RetCor_01.png"), h=800, w=1600)
+  dev.off()
+  xset.group2 <- xcms::group(xset.2, method="density", bw=bw_param[2], mzwid=mzwid_param, minfrac=mzwid_param)
+  xset.3 <- xcms::retcor(xset.group2, method="obiwarp", profStep=profStep_param, plottype="deviation")
+  dev.copy(png, paste0(Results.path.rtgraph, "RetCor_02.png"), h=800, w=1600)
+  dev.off()
+  xset.group.4 <- xcms::group(xset.3, method="density", bw=bw_param[3], mzwid=mzwid_param, minfrac=mzwid_param)
+  xset.filled <- xcms::fillPeaks(xset.group.4)
+  return(xset.filled) ## Output results
+
+  ## Generate Data.matrix, Samples and Variables metadata
+  write.table(peakTable(xset.default.4), file=paste0(Results.path.root, "Peak_Table.csv"), sep=";", col.names=NA)
+  Sample.Metadata.D <- data.frame(row.names=Sample.Metadata[,1], Sample.Metadata[2:ncol(Sample.Metadata)])
+  Data <- list()
+  Data[[1]] <- t(peakTable(xset.filled)[(ncol(peakTable(xset.filled))-nrow(xset.filled@phenoData)+1):ncol(peakTable(xset.filled))])
+  names(Data[1]) <- "Datamatrix"
+  Data[[2]] <- subset(Sample.Metadata.D, rownames(Sample.Metadata.D) %in% rownames(xset.filled@phenoData))
+  names(Data[2]) <- "Sample.metadata"
+  Data[[3]] <- peakTable(xset.filled)[1:(ncol(peakTable(xset.filled))-nrow(xset.filled@phenoData))]
+  names(Data[3]) <- "Variable.metadata"
+
+  ## internal STD 1 133.1062 rt 60  Leucine-5,5,5,-d3
+  ## internal STD 2 206.1014 rt 151 L-tryptophan-2,3,3-d3
+  ## internal STD 3 179.0874 rt 375 Indole-2,4,5,6,7-d5-3-acetic acid
+  ## internal STD 4 281.3265 rt 566 1,14-tetradecanedioic-d24 acid
+  group.id.p <- as.numeric(c(rownames(subset(Data[[3]], Data[[3]][, "mz"] >= min(xcms:::ppmDev(133.1062, 5)) & Data[[3]][, "mz"] <= max(xcms:::ppmDev(133.1062, 5)))),
+                             rownames(subset(Data[[3]], Data[[3]][, "mz"] >= min(xcms:::ppmDev(206.1014, 5)) & Data[[3]][, "mz"] <= max(xcms:::ppmDev(206.1014, 5)))),
+                             rownames(subset(Data[[3]], Data[[3]][, "mz"] >= min(xcms:::ppmDev(179.0874, 5)) & Data[[3]][, "mz"] <= max(xcms:::ppmDev(179.0874, 5)))),
+                             rownames(subset(Data[[3]], Data[[3]][, "mz"] >= min(xcms:::ppmDev(281.3265, 5)) & Data[[3]][, "mz"] <= max(xcms:::ppmDev(281.3265, 5))))))
+
+  temp.eic.r <- getEIC(xset.default.4, groupidx=group.id.p, rt="raw")
+  temp.eic.c <- getEIC(xset.default.4, groupidx=group.id.p, rt="corrected")
+  temp.n <- length(group.id.p)
+  par(mfrow=c(temp.n,2))
+  for (i in 1:length(group.id.p)){
+    plot(temp.eic.r, xset.default.4, groupidx=i, main="RAW")
+    plot(temp.eic.c, xset.default.4, groupidx=i, main="Corrected")
+  }
+  dev.copy(png, paste0(Results.path.root, "EIC.STD.peaks.png"), h=1400, w=1000)
+  dev.off()
+  par(mfrow=c(1,1))
+
+  ## Write table with STDs only and ppm deviation
+  STDs.results <- subset(Data[[3]], rownames(Data[[3]]) %in% group.id.p)
+  STDs.results$ppm <- (STDs.results$mzmax - STDs.results$mzmin)/(STDs.results$mz/1000000)
+  STDs.results
+  write.table(STDs.results, file=paste0(Results.path.root, "Results_STDs.csv"), sep=";", col.names = NA)
+
+  ## Perform PCA
+  library(ropls)
+  ACP.results.list <- list()
+  ACP.results.list[["ACP"]] <- opls(Data[[1]], predI=NA, plotL=F)
+  for (i in 1:length(ACP.results.list)){
+    Data.pca <- ACP.results.list[[i]]
+    png(filename=paste0(Results.path.pca, names(ACP.results.list[i]),".png"), width=800, height=1200, units="px", res=150)
+    par(mfrow=c(3,2))
+    plot(Data.pca, typeVc="overview", parDevNewL=F)
+    plot(Data.pca, typeVc="x-loading", parDevNewL=F)
+    plot(Data.pca, typeVc="x-score", parDevNewL=F)
+    plot(Data.pca, typeVc="outlier", parDevNewL=F)
+    plot(Data.pca, typeVc="correlation", parDevNewL=F)
+    dev.off()
+    rm(Data.pca)
+  }
+  x <- -4
+  y <- -7
+  png(filename=paste0(Results.path.pca, "ACP_Ellipses.png"), width=900, height=900, units="px", res=100)
+  par(mfrow=c(2,2))
+  for (i in Grouping.factor){
+    Data.pca <- ACP.results.list[[1]]
+    temp.factor <- Data[[2]][,i]
+    temp.factor.names <- names(Data[[2]][i])
+    plot(Data.pca, typeVc="x-score", parAsColFcVn=addNA(as.factor(temp.factor)), parEllipses=F, parDevNewL=F)
+    text(x,y,temp.factor.names)
+  }
+  dev.off()
+}
+
+
+
