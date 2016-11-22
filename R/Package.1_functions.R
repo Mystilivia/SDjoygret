cad35200736eed9b98ec9e14dd793031f5f83e43
Mystilivia <mystilivia@gmail.com>
2016-11-22 11:02:19 +0100

    debug summary plot

--- R/Package.1_functions.R
+++ R/Package.1_functions.R
@@ -1021,7 +1021,7 @@ dlist.summary.2 <- function(dlist,
       group_by(Measure)
     yline <- rbind(data.frame("Measure" = "Skew", "yint" = c(0,-1,1), "color_var" = c("black", "red", "blue")),
                    data.frame("Measure" = "Perc_Zero", "yint" = c(50, 0, 100), "color_var" = c("black", "red", "blue")))
-    plot1 <- ggplot(temp.plot, aes(reorder(variable, Avg), value, ymin = 0, ymax = value, color = Measure)) +
+    plot1 <- ggplot(temp.plot, aes(reorder(variable, value), value, ymin = 0, ymax = value, color = Measure)) +
       geom_hline(data = yline, aes(yintercept = yint), color = yline$color_var, linetype = 2, alpha = alpha) +
       geom_pointrange(alpha = alpha, size = 0.02) +
       facet_grid(Measure~., scale = "free_y") +
