3a0e3f90ba3f12a283126f90190994d3cb87ab43
Mystilivia <mystilivia@gmail.com>
2016-11-29 09:00:51 +0100

    debug

--- R/Package.1_functions.R
+++ R/Package.1_functions.R
@@ -636,18 +636,18 @@ check.list.format <- function (dlist, rownamesL = F, tibbleL = F) {
   require("tidyverse")
   if(!is.list(dlist)){stop("Data should be a list with (1) Datamatrix (2) Sample.Metadata (3) Variable.Metadata")}
   temp.data.str <- dlist.class(dlist)
-  if(all(temp.data.str[,class.d.t] == F) & all(temp.data.str[,class.t] == F) & all(temp.data.str[,class.d.f] == F)) {stop("List levels should be data.frame, tibble or data.table") }
+  if(all(temp.data.str[,"class.d.t"] == F) & all(temp.data.str[,"class.t"] == F) & all(temp.data.str[,"class.d.f"] == F)) {stop("List levels should be data.frame, tibble or data.table") }
   if(rownamesL == F){
     if(!identical(dlist[[1]][[1]], dlist[[2]][[1]])){stop("Datamatrix rownames should be identical of Sample.Metadata rownames")}
-    if(!identical(names(data.frame(dlist[[1]])[-1]), as.character(dlist[[3]][[1]]))){stop("Datamatrix colnames should be identical of Variable.Metadata rownames")}
+    if(!identical(names(data.frame(dlist[[1]]))[-1], as.character(dlist[[3]][[1]]))){stop("Datamatrix colnames should be identical of Variable.Metadata rownames")}
   } else if(rownamesL == T){
     if(!identical(rownames(dlist[[1]]), rownames(dlist[[2]]))){stop("Datamatrix rownames should be identical of Sample.Metadata rownames")}
     if(!identical(names(dlist[[1]]), rownames(dlist[[3]]))){stop("Datamatrix colnames should be identical of Variable.Metadata rownames")}
     message("Data are well structured.")
     return(dlist)
   }
-  if(all(temp.data.str[,class.t] == T)) {message("Data are stored as tibbles, well done !")
-  } else if(all(temp.data.str[,class.d.t] == T)) { ## all are data.table
+  if(all(temp.data.str[,"class.t"] == T)) {message("Data are stored as tibbles, well done !")
+  } else if(all(temp.data.str[,"class.d.t"] == T)) { ## all are data.table
     if(tibbleL == F) {stop("Data are stored as data.table, to convert to tibbles : set args tibbleL to TRUE.")}
     if(tibbleL == T) {
       if(rownamesL == T) {stop("Data are stored as data.table, there shouldn't be any rownames, please check and set argument rownamesL to FALSE.")}
@@ -658,7 +658,7 @@ check.list.format <- function (dlist, rownamesL = F, tibbleL = F) {
         return(temp.list)
       }
     }
-  } else if(all(temp.data.str[,class.d.f] == T)) { ## all are data.frame
+  } else if(all(temp.data.str[,"class.d.f"] == T)) { ## all are data.frame
     if(tibbleL == F) {stop("Data are stored as data.frame, to convert to tibbles : set args tibbleL to TRUE.")}
     if(tibbleL == T) {
       if(rownamesL == T) {
